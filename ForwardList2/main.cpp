74e85fa9e17feeb64d1c01d37f672c6b18bfbcef
Machk_yz84@top-academy.ru <Machk_yz84@top-academy.ru>
2023-10-04 18:26:30 +0300

    2DList in progress...

--- ForwardList2/main.cpp
+++ ForwardList2/main.cpp
@@ -3,7 +3,53 @@ using namespace std;
 
 
 class Element {
+	int Data;
+	Element* pNext;
+	Element* pPrev;
+	static int count;
+public:
+	Element(int Data, Element* pNext = nullptr, Element* pPrev = nullptr) : Data(Data), pNext(pNext), pPrev(pPrev) {
+		count++;
+		cout << "EConstructor:\t" << this << endl;
+	}
+	~Element() {
+		count--;
+		cout << "EDestructor:\t" << this << endl;
+	}
 
+	friend class ForwardList;
+
+};
+
+int Element::count = 0;
+
+class ForwardList {
+	Element* Head;
+	Element* Tail;
+	unsigned int size;
+public:
+
+	ForwardList() {
+		this->Head = nullptr;
+		this->Tail = nullptr;
+		this->size = 0;
+		cout << "LConstructor:\t" << this << endl;
+	}
+	~ForwardList() {
+		cout << "LDestructor:\t" << this << endl;
+	}
+
+	//                     Adding elements
+
+	void push_front(int Data) {
+		Element* New = new Element(Data);
+		New->pPrev = nullptr;
+		New->pNext = Head;
+		Head = New;
+		
+		size++;
+
+	}
 };
 
 void main() {
